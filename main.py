26032cca5062b54635805dfeacbebc7b78e625fd
xtaodada <xtao@xtao.de>
2025-04-02 16:13:08 +0800

    chore: use uv

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Query
+from starlette.responses import RedirectResponse
+
 from utils import get_music
 
 app = FastAPI()
@@ -11,3 +13,8 @@ async def search(
     keyword: str = Query(..., title="The name of a song")
 ):
     return await get_music(service, keyword)
+
+
+@app.get("/")
+async def root():
+    return RedirectResponse(url="https://music.163.com", status_code=302)
